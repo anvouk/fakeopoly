99a4c393797d4d0a9514981f18672c26b1d23668
Andrea Vouk <andrew.vouk@gmail.com>
2024-10-18 17:07:36 +0200

    Update to angular 18 and remove zone.js

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { AppComponent } from './app/app.component';
 import { provideRouter, withHashLocation } from "@angular/router";
 import { routes } from './app/app.routes';
-import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { enableProdMode, importProvidersFrom, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { environment } from './environments/environment';
@@ -14,5 +14,6 @@ bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes, withHashLocation()),
     importProvidersFrom(BrowserAnimationsModule),
+    provideExperimentalZonelessChangeDetection()
   ],
 }).catch(err => console.error(err));
